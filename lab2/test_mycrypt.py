45e6de432e2a468c0b6ff05c1becd7a17ce435ed
Sahe00 <71127023+Sahe00@users.noreply.github.com>
2022-11-09 20:41:53 +0200

    Update test_mycrypt.py

--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -48,13 +48,13 @@ def test_invalid_types(invalid_input):
     with pytest.raises(TypeError):
         mycrypt.encode(invalid_input)
 
-"""
-@pytest.mark.parametrize("invalid_input", ["x"*2000])
+        
+@pytest.mark.parametrize("invalid_input", ["3"*4000, "a"*2000])
 def test_invalid_types(invalid_input):
     '''Invalid input parameter length should raise ValueError'''
     with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
-"""
+
 
 def test_timing():
     '''Test whether encoding runs in approximately constant time, repetitions
